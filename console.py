19fbff26e263701f9d7833e0b4f93aadab6c7653
Alex <cptnsniper@gmail.com>
2023-02-23 18:39:46 -0500

    rotating sphere

--- console.py
+++ console.py
@@ -4,7 +4,8 @@ x = 119
 y = 30
 aspect = x / y
 pixel_aspect = 19 / 39
-shades = " .:!/r(l1Z4H9W8$@"
+# shades = " .:!/r(l1Z4H9W8$@"
+shades = " .*/?9#&@"
 
 def clamp(value, min1, max1):
     return(min(max(value, min1), max1))
@@ -33,31 +34,33 @@ def subtract(vec1, vec2):
     return (vec1[0] - vec2[0], vec1[1] - vec2[1], vec1[2] - vec2[2])
 
 def draw_ray():
-    draw_string = ""
-    for i in range(y):
-        for j in range(x):
-            u = j / x * 2 - 1
-            v = i / y * 2 - 1
-            u *= aspect * pixel_aspect
+    for t in range(10000):
+        draw_string = ""
+        for i in range(y):
+            for j in range(x):
+                u = j / x * 2 - 1
+                v = i / y * 2 - 1
+                u *= aspect * pixel_aspect
 
-            sphere_origin = (0, 0, 5)
-            light = (-10, 10, 2)
-            pixel = " "
+                sphere_origin = (0, 0, 5)
+                radius = 7
+                light = (sin(t * 0.01) * 5, -5, cos(t * 0.01) * 5)
+                pixel = " "
 
-            ray = (u * 10, v * 10, 0)
-            hit = False
-            for ii in range(10):
-                ray = (u * 10, v * 10, sphere_dist(ray, sphere_origin, 5))
-                if (sphere_dist(ray, sphere_origin, 5) < 1):
-                    hit = True
-                    break
-            if (hit):
-                pixel = shades[round(map(angle(subtract(ray, sphere_origin), light), 0, pi, 0, len(shades) - 1))]
-                # break
-            draw_string += pixel
-                
-        draw_string += "\n"
-    print(draw_string)
+                ray = (u * 10, v * 10, 0)
+                hit = False
+                for ii in range(10):
+                    ray = (u * 10, v * 10, sphere_dist(ray, sphere_origin, radius))
+                    if (sphere_dist(ray, sphere_origin, radius) < 1):
+                        hit = True
+                        break
+                if (hit):
+                    pixel = shades[round(map(angle(subtract(ray, sphere_origin), light), 0, pi, len(shades) - 1, 0))]
+                    # break
+                draw_string += pixel
+                    
+            draw_string += "\n"
+        print(draw_string)
 
 
 # draw()
