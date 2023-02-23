203bb80f35883ca70ed8ff5eb74b2ef62a06246e
Alex <cptnsniper@gmail.com>
2023-02-22 22:05:24 -0500

    shading

--- console.py
+++ console.py
@@ -1,4 +1,4 @@
-from math import pow, sin, cos, sqrt
+from math import pow, sin, cos, sqrt, acos, pi
 
 x = 119
 y = 30
@@ -6,30 +6,31 @@ aspect = x / y
 pixel_aspect = 19 / 39
 shades = " .:!/r(l1Z4H9W8$@"
 
-def clamp(value, mini, maxi):
-    return(min(max(value, mini), maxi))
+def clamp(value, min1, max1):
+    return(min(max(value, min1), max1))
 
 def dist(vec1, vec2):
     return sqrt(pow(vec2[0] - vec1[0], 2) + pow(vec2[1] - vec1[1], 2) + pow(vec2[2] - vec1[2], 2))
 
-def draw():
-    for t in range(10000):
-        draw_string = ""
-        for i in range(y):
-            for j in range(x):
-                u = j / x * 2 - 1
-                v = i / y * 2 - 1
-                u *= aspect * pixel_aspect
+def mag(vec):
+    return dist((0, 0, 0), vec)
 
-                u += sin(t * 0.02)
-                v += cos(t * 0.02)
+def dot(vec1, vec2):
+    return vec1[0] * vec2[0] + vec1[1] * vec2[1] + vec1[2] * vec2[2]
 
-                color = clamp((1 - (pow(u, 2) + pow(v, 2)) / (pow(1 * aspect * pixel_aspect, 2) + 1)) * (len(shades) - 1), 0, len(shades) - 1)
+def angle(vec1, vec2):
+    return acos(dot(vec1, vec2) / (mag(vec1) * mag(vec2)))
 
-                draw_string += shades[round(color)]
-                
-            draw_string += "\n"
-        print(draw_string)
+def map(value, min_in, max_in, min_out, max_out):
+    return min_out + ((max_out - min_out) / (max_in - min_in) * (value - min_in))
+
+def sphere_dist(point, centre, radius):
+    return dist(point, centre) - radius
+
+def add(vec1, vec2):
+    return (vec1[0] + vec2[0], vec1[1] + vec2[1], vec1[2] + vec2[2])
+def subtract(vec1, vec2):
+    return (vec1[0] - vec2[0], vec1[1] - vec2[1], vec1[2] - vec2[2])
 
 def draw_ray():
     draw_string = ""
@@ -39,15 +40,19 @@ def draw_ray():
             v = i / y * 2 - 1
             u *= aspect * pixel_aspect
 
-            # camera_start = (u, v, 0)
             sphere_origin = (0, 0, 5)
+            light = (-10, 10, 2)
             pixel = " "
 
-            # for i in range(100):
             ray = (u * 10, v * 10, 0)
-            ray = (u * 10, v * 10, dist(ray, sphere_origin))
-            if (dist(ray, sphere_origin) < 5):
-                pixel = "@"
+            hit = False
+            for ii in range(10):
+                ray = (u * 10, v * 10, sphere_dist(ray, sphere_origin, 5))
+                if (sphere_dist(ray, sphere_origin, 5) < 1):
+                    hit = True
+                    break
+            if (hit):
+                pixel = shades[round(map(angle(subtract(ray, sphere_origin), light), 0, pi, 0, len(shades) - 1))]
                 # break
             draw_string += pixel
                 
@@ -56,4 +61,5 @@ def draw_ray():
 
 
 # draw()
+# print(map(angle((-1, 0, 0), (1, 0, 0)), 0, pi, 0, 180))
 draw_ray()
